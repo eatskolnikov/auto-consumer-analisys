7fe4638eb002582c54dca3fae73ba84fa4f4f36b
Enmanuel Toribio <eats007@gmail.com>
2012-11-05 17:35:29 -0400

    Merged ethernet and bluetooth into a single file

--- src/arduino/ethernet/ethernet.ino
+++ src/arduino/ethernet/ethernet.ino
@@ -1,15 +1,15 @@
 #include <SPI.h>        
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-//byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x1A, 0x40 };
-byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x53, 0x40 };
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x1A, 0x40 };
+//byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x53, 0x40 };
 IPAddress ip(10, 0, 0, 21);
+//IPAddress ip(10, 0, 0, 22);
 IPAddress server_ip(10, 0, 0, 107);
 
-unsigned int localPort = 8888;      // local port to listen on
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+unsigned int localPort = 8888;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 EthernetUDP Udp;
 
 void setup() {
@@ -20,7 +20,6 @@ void setup() {
 
 void sendmessage(char* message)
 {
-    // send a reply, to the IP address and port that sent us the packet we received
     Udp.beginPacket(server_ip, localPort);
     Udp.write(message);
     Udp.endPacket();
--- src/arduino/program_final/program_final.ino
+++ src/arduino/program_final/program_final.ino
@@ -1,13 +1,12 @@
+#include <SoftwareSerial.h>
 #include <SPI.h>        
 #include <Ethernet.h>
 #include <EthernetUdp.h>
-#include <SoftwareSerial.h>
 
-#define rxPin 3
+#define rxPin 5
 #define txPin 4
 #define ledPin 13
 #define baud_rate 38400
-#define baud_rate_bt 9600
 
 #define BTINIT 0
 #define BTSETCLASS 1
@@ -15,84 +14,74 @@
 #define BTADDRESS 3
 #define BTINQLOOP 4
 
-//#define MAC { 0x90, 0xA2, 0xDA, 0x0D, 0x53, 0x40 }
-#define MAC { 0x90, 0xA2, 0xDA, 0x00, 0x1A, 0x40 } 
-//#define IP 10, 0, 0, 101
-#define IP 10, 0, 0, 102
-#define SERVER_IP 10, 0, 0, 21
-#define LOCALPORT 8888
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x1A, 0x40 };
+//byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0x53, 0x40 };
+IPAddress ip(10, 0, 0, 21);
+//IPAddress ip(10, 0, 0, 22);
+IPAddress server_ip(10, 0, 0, 107);
 
-byte mac[] = MAC;
-IPAddress ip(IP);
-IPAddress server_ip(SERVER_IP);
-unsigned int localPort = LOCALPORT;   // local port to listen on
-char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+unsigned int localPort = 8888;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 EthernetUDP Udp;
 
-SoftwareSerial bluetoothInterface = SoftwareSerial(rxPin, txPin);
+SoftwareSerial bluetoothInterface(rxPin, txPin);
+
 int checked = 0;
-String inputString = "", addr="";
+String inputString = "";
 int cmd = BTINIT;
 
 void setup() {
-  // start the Ethernet and UDP:
   Ethernet.begin(mac,ip);
   Udp.begin(localPort);
-
-  //setup the bluetooth
-  bluetoothInterface.begin(baud_rate);
+  
   Serial.begin(baud_rate);
-  while (!Serial);
+  bluetoothInterface.begin(baud_rate);
   bluetoothInterface.write("AT+INIT\r\n");
-  addr.reserve(30);
   inputString.reserve(512);
+  delay(1000);
 }
-void sendmessage(char* message)
-{
-    Udp.beginPacket(server_ip, localPort);
-    Udp.write(message);
-    Udp.endPacket();
-}
+
 void loop() {
 }
 
 void serialEvent() {
-  //inputString = "";
   inputString = "";
-  
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
-    inputString += inChar;
-  }
-  if(!(inputString == ""))
+  if(Serial.available())
   {
-    Serial.print(addr);
-    //Serial.print(inputString);
-    Udp.beginPacket(server_ip, localPort);
-    char *arr;
-    addr.toCharArray(arr, addr.length(), addr.length());
-    Udp.write(arr);
-    Udp.endPacket();
-    addr = "";
+    char inChar = ' ';
+    do {
+      inChar = (char)Serial.read();
+      if(inChar >= 32 && inChar <127)
+        inputString += inChar;
+    }
+    while(inChar !='\n');
+    if(inputString != "OK")
+    {
+      char charBuff[inputString.length()];
+      inputString.toCharArray(charBuff, inputString.length());
+      Udp.beginPacket(server_ip, localPort);
+      Udp.write(charBuff);
+      Udp.endPacket();
+      Serial.println(inputString);
+    }
+    if(cmd == -1)
+      cmd = BTINQ;
   }
   switch (cmd)
   {
-    case BTINIT:
-      bluetoothInterface.write("AT+ROLE=1\r\n");
-      cmd = BTSETCLASS;
+  case BTINIT:
+    bluetoothInterface.write("AT+ROLE=1\r\n");
+    cmd = BTSETCLASS;
     break;
-    case BTSETCLASS:
-      bluetoothInterface.write("AT+INQM=1,9,48\r\n");
-      cmd = BTINQ;
+  case BTSETCLASS:
+    bluetoothInterface.write("AT+CLASS=0\r\n");
+    bluetoothInterface.write("AT+INQM=1,9,10\r\n");
+    bluetoothInterface.write("AT+IPSCAN=128,64,1024,512\r\n");
+    cmd = BTINQ;
     break;
-    case BTINQ:
-      bluetoothInterface.write("AT+ADDR?\r\n");
-      cmd = BTADDRESS;
-      break;
-    case BTADDRESS:
-      addr += inputString;
-      cmd = BTINQLOOP;
-    default:
-      bluetoothInterface.write("AT+INQ\r\n");
+  case BTINQ:
+    bluetoothInterface.write("AT+INQ\r\n");
+    cmd = -1;
+    break;
   }
 }
