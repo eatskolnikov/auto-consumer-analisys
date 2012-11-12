ccc5a55d5e95e6abc853251ccc6c9f77763ddaf3
Enmanuel Toribio <eats007@gmail.com>
2012-11-11 21:46:10 -0400

    La aplicacion que recibe los paquetes esta completa

--- src/app/ACAPackagesListener/ACAPackagesListener/Program.cs
+++ src/app/ACAPackagesListener/ACAPackagesListener/Program.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using ACAPackagesListener.API;
+using Bll.AutoConsumerAnalisys;
+using Lrrc.Sys.Data;
+
 
 namespace ACAPackagesListener
 {
     class Program
     {
+        static Connection connection;
+        static AdmPackages admPackages;
         static void Main(string[] args)
         {
             IListenToPackages packagesListener = new UDPPackagesListener(8888);
-            packagesListener.onPackageReceived += new PackageReceivedHandler(packagesListener_onPackageReceived);
+            packagesListener.onPackageReceived += packagesListener_onPackageReceived;
             packagesListener.StartListening();
-            while (true) ;
+            connection = new Connection(Globals.ConnectionString);
+            admPackages = AdmPackages.Crear(ref connection);
+            while (true)
+            {
+            }
         }
 
         static void packagesListener_onPackageReceived(object o, PackageReceivedEventArgs e)
@@ -21,6 +32,14 @@ namespace ACAPackagesListener
             Console.WriteLine("Received message");
             Console.WriteLine(e.sourceIp);
             Console.WriteLine(e.message);
+            admPackages.Insertar(0,e.sourceIp,e.message,false,true);
+            try{
+                admPackages.Sincronizar();
+                admPackages.GetTblLogic.Rows.Clear();
+            }catch(Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
         }
     }
 }
