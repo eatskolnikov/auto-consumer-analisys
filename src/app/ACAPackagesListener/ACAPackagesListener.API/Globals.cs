df48066a896b98484826b8867c4a8f2c8810b382
Enmanuel Toribio <eats007@gmail.com>
2013-04-19 01:33:41 -0400

    Connection strings are now made with config files

--- src/app/ACAPackagesListener/ACAPackagesListener.API/Globals.cs
+++ src/app/ACAPackagesListener/ACAPackagesListener.API/Globals.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 
@@ -14,7 +16,15 @@ namespace ACAPackagesListener.API
 
         public static string ConnectionString
         {
-            get { return @"Data Source=AMAZONA-BJ2E5Q9;Initial Catalog=autoconsumeranalisys;Integrated Security=True"; }
+            get
+            {
+                var connectionStringsSection = ConfigurationManager.GetSection("connectionStrings") as ConnectionStringsSection;
+                if (connectionStringsSection != null)
+                {
+                    return connectionStringsSection.ConnectionStrings[0].ConnectionString;;
+                }
+                return "";
+            } //@"Data Source=AMAZONA-BJ2E5Q9;Initial Catalog=autoconsumeranalisys;Integrated Security=True"; }
         }
     }
 }
