0271d3ae0e7c3979fb45e400cd495ccf9d2cf75b
Enmanuel Toribio <eats007@gmail.com>
2013-04-24 17:50:21 -0400

    Finished service

--- src/app/PackagesParserServer/PackagesParserServer/Program.cs
+++ src/app/PackagesParserServer/PackagesParserServer/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Threading;
 
 namespace PackagesParserServer
 {
@@ -9,6 +13,26 @@ namespace PackagesParserServer
     {
         static void Main(string[] args)
         {
+            while(true){
+                Thread.Sleep(5000);
+                RunParser();
+            }
+        }
+        static void RunParser()
+        {
+            try
+            {
+                var connection = new SqlConnection(ConfigurationManager.ConnectionStrings[0].ConnectionString);
+                connection.Open();
+                var command = new SqlCommand("Spc_Parse_Packages", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch
+            {
+                
+            }
         }
     }
 }
