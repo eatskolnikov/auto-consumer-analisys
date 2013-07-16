f35e69c96cf4242fac7ede90a1256f2728b09d0e
Enmanuel Toribio <eats007@gmail.com>
2013-07-15 20:44:37 -0400

    adding mvc3

---
+++ src/site/vs/acaweb/Content/js/maphelpers.js
@@ -0,0 +1,11 @@
+﻿var showMessage = function (message) {
+    if (typeof (message) == 'undefined') { return; }
+    else {
+        $('#modalDialog .modal-body')[0].innerHTML = message;
+        $('#modalDialog').modal('show');
+    }
+};
+
+var showHelp = function () {
+    $('#modalDialog').modal({ show: true, remote: 'http://ec2-23-20-227-175.compute-1.amazonaws.com/Help.aspx' });
+};
