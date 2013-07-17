677d320a6ccf0d7d6a409aecbb49f242b14f035c
Enmanuel Toribio <etoribio@intellisys.com.do>
2013-07-17 00:19:44 -0400

    Renamed namespace

---
+++ src/app/ACAPackagesListener/ACAPackagesListener.API/Models/Entities/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ACAPackagesListener.API.Models.Entities
+{
+    [Serializable]
+    public class Device
+    {
+        public virtual Int32 DeviceId { get; set; }
+        public virtual string Ip { get; set; }
+        public virtual string LatLng { get; set; }
+        public virtual string Description { get; set; }
+        public virtual bool Activo { get; set; }
+        public virtual DateTime Created { get; set; }
+    }
+}
