edde6a155fb56b3359e656efe7c64a7d302cec10
Enmanuel Toribio <eats007@gmail.com>
2013-07-20 12:58:54 -0400

    Routes report

---
+++ src/site/vs/acaweb/Content/js/routes-report.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    var printRoutes = function () {
+        $.ajax({
+            url: base_url + 'Packages/Get/',
+            method: 'GET',
+            dataType: 'json',
+            success: function (packages) {
+                console.log(packages);
+                var l = packages.length;
+                console.log(l);
+                var routes = {};
+                for (var i = 0; i < l; i++) {
+                    var parsedPackage = packages[i];
+                    if (typeof (routes[parsedPackage.MAC]) == 'undefined') {
+                        routes[parsedPackage.MAC] = Array();
+                    }
+                    var latLng = parsedPackage.LatLng.replace('(', '').replace(')', '').replace(' ', '').split(',');
+                    var packageLatLng = new google.maps.LatLng(latLng[0], latLng[1]);
+                    routes[parsedPackage.MAC].push(packageLatLng);
+                }
+                for (var route in routes) {
+                    var path = new google.maps.Polyline({
+                        path: routes[route],
+                        strokeColor: '#FF0000',
+                        strokeOpacity: 1.0,
+                        strokeWeight: 2
+                    });
+                    path.setMap(map);
+                }
+            }
+        }).fail(function (jqXHR, textStatus) {
+            alert("Error cargando las rutas");
+        });
+    };
+    printRoutes();
+});
---
+++ src/site/vs/acaweb/Controllers/PackagesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ACAPackagesListener.API.Models.Repositories;
+
+namespace acaweb.Controllers
+{
+    public class PackagesController : Controller
+    {
+        private readonly IParsedPackageRepository _parsedPackagesRepository;
+
+        public PackagesController()
+        {
+            _parsedPackagesRepository = new NHParsedPackageRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var packages = _parsedPackagesRepository.GetAll();
+            return Json(packages, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Get(string MAC = "")
+        {
+            var packages = _parsedPackagesRepository.GetAll();
+            if(!String.IsNullOrEmpty(MAC))
+            {
+                packages = packages.Where(x => x.MAC == MAC);
+            }
+            return Json(packages.OrderBy(x => x.PackageDate), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
