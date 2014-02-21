ff973cdb090ff7922ce0e84a213fe1bb0db31c33
Enmanuel Toribio <eats007@gmail.com>
2014-02-21 02:27:22 -0400

    Fixed map rendering issue

--- src/site/vs/acaweb/Content/js/mapbasic.js
+++ src/site/vs/acaweb/Content/js/mapbasic.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     var map_rows = 4;
     var tile_dimension = 168;
     var mapCenter = new google.maps.LatLng(2, -61);
-    var maptiler = new google.maps.ImageMapType({
+    var mallTypeOptions = {
         getTileUrl:
             function (coord) {
                 var x = coord.x % map_cols;
@@ -20,14 +20,27 @@ $(document).ready(function () {
                 return base_url + "Content/img/map/tiles/" + y + "/" + x + ".png";
             },
         tileSize: new google.maps.Size(tile_dimension, tile_dimension),
-        isPng: true
-    });
-    var mapOptions = { center: mapCenter, zoom: 3, maxZoom: 3, minZoom: 3, disableDefaultUI: true, draggable:false };
+        maxZoom: 9,
+        minZoom: 0,
+        name: 'Mall'
+    };
+
+    var mallMapType = new google.maps.ImageMapType(mallTypeOptions);
+    var mapOptions = {
+        center: mapCenter, zoom: 3, maxZoom: 3, minZoom: 3, disableDefaultUI: true, draggable: false,
+        mapTypeControlOptions: {
+            mapTypeIds: ['mall']
+        }
+    };
     var strictBounds = new google.maps.LatLngBounds(new google.maps.LatLng(0, 0), new google.maps.LatLng(0, 0));
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+
+    map.mapTypes.set('mall', mallMapType);
+    map.setMapTypeId('mall');
+
     var defaultBounds = map.getBounds();
     var defaultCenter = map.getCenter();
-    map.overlayMapTypes.insertAt(0, maptiler);
+   // map.overlayMapTypes.insertAt(0, maptiler);
     google.maps.event.addListener(map, 'drag', function (e) {
         if (strictBounds.contains(map.getCenter())) return;
         var c = map.getCenter(), x = c.lng(), y = c.lat(),
