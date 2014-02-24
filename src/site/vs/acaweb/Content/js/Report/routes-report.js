7c27c65c5180e6434a24ddaf506ff2920782ad2f
Enmanuel Toribio <eats007@gmail.com>
2014-02-24 17:50:33 -0400

    Adding direction to text

--- src/site/vs/acaweb/Content/js/Report/routes-report.js
+++ src/site/vs/acaweb/Content/js/Report/routes-report.js
@@ -1,6 +1,7 @@
 ﻿
 $(function () {
     var currentArrows = [];
+    var currentLabels = [];
     var paths = {};
     var packagesurl = base_url + 'Packages/Get/';
     var lineSymbol = { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW };
@@ -13,7 +14,7 @@ $(function () {
             success: function (packagesGroupedByMac) {
                 for (var macIdx in packagesGroupedByMac) {
                     var lastLatLng = '';
-                    var currLatLng = '';
+                    var currLatLng;
                     var packages = packagesGroupedByMac[macIdx];
                     for (var packageIdx in packages) {
                         var pack = packages[packageIdx];
@@ -33,18 +34,33 @@ $(function () {
                         var end = endPoint.replace('(', '').replace(')', '').replace(' ', '').split(',');
                         var endLatLng = new google.maps.LatLng(end[0], end[1]);
                         var lineCoordinates = [startLatLng, endLatLng];
+                        var labelLat = (endLatLng.lat() + startLatLng.lat()) / 2;
+                        var labelLng = (endLatLng.lng() + startLatLng.lng()) / 2;
+                        var direction = "→"; 
+                       // if (startLatLng.lat() > endLatLng.lat())
+                        //    direction = "←";
+                        //
+                        var label = new MapLabel({
+                            map: map,
+                            text: paths[startPoint][endPoint] + " personas " + direction,
+                            position: new google.maps.LatLng(labelLat, labelLng)
+                        });
                         var line = new google.maps.Polyline({
                             path: lineCoordinates,
                             icons: [{icon: lineSymbol, offset: '100%'}],
                             map: map, strokeWeight: 2, strokeOpacity: 0.8
                         });
+                        line.label = label;
                         google.maps.event.addListener(line, "mouseover", function () {
                             this.setOptions({ strokeColor: "#FF0000" });
+                            this.label.set('fontColor', "#FF0000");
                         });
                         google.maps.event.addListener(line, "mouseout", function () {
                             this.setOptions({ strokeColor: "#000000" });
+                            this.label.set('fontColor', "#000000");
                         });
                         currentArrows.push(line);
+                        currentLabels.push(label);
                     }
                 }
             }
@@ -56,6 +72,8 @@ $(function () {
     });
     var reloadLoop = function () {
         for (var arrow in currentArrows) { currentArrows[arrow].setMap(null); }
+        for (var label in currentLabels) { currentLabels[label].setMap(null); }
+        paths = [];
         reloadReport(printRoutes, packagesurl);
         setTimeout(reloadLoop, parseInt($("#refreshingTime").val()) * 1000);
     };
--- src/site/vs/acaweb/Content/js/mapbasic.js
+++ src/site/vs/acaweb/Content/js/mapbasic.js
@@ -4,68 +4,34 @@ var ShowAlert = function (message, type) {
     $("#showAlert").attr('class', 'alert alert-'+type);
     $("#showAlert").html('<button type="button" class="close" data-dismiss="alert">×</button>El dispositivo fue modificado satisfactoriamente');
 };
-var routeColors = ['#C0C0C0', '#808080', '#000000', '#FF0000', '#800000', '#FFFF00', '#808000', '#00FF00', '#008000', '#00FFFF', '#008080', '#0000FF', '#000080', '#FF00FF', '#800080'];
 $(document).ready(function () {
     var map_cols = 8;
     var map_rows = 4;
     var tile_dimension = 168;
     var mapCenter = new google.maps.LatLng(2, -61);
     var mallTypeOptions = {
-        getTileUrl:
-            function (coord) {
+        getTileUrl: function (coord, zoom) {
                 var x = coord.x % map_cols;
                 var y = coord.y % map_rows;
                 if (x < 0 || y < 0) { return ""; }
                 if (y < 0) y = y * -1;
-                return base_url + "Content/img/map/tiles/" + y + "/" + x + ".png";
+                return base_url + "Content/img/map/3/tiles/" + y + "/" + x + ".png";
             },
         tileSize: new google.maps.Size(tile_dimension, tile_dimension),
-        maxZoom: 9,
-        minZoom: 0,
-        name: 'Mall'
+        maxZoom: 3,minZoom: 3, name: 'Mall'
     };
 
     var mallMapType = new google.maps.ImageMapType(mallTypeOptions);
     var mapOptions = {
-        center: mapCenter, zoom: 3, maxZoom: 3, minZoom: 3, disableDefaultUI: true, draggable: false,
+        center: mapCenter, zoom: 3, disableDefaultUI: true, draggable: false,
         mapTypeControlOptions: {
             mapTypeIds: ['mall']
         }
     };
-    var strictBounds = new google.maps.LatLngBounds(new google.maps.LatLng(0, 0), new google.maps.LatLng(0, 0));
     map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-
     map.mapTypes.set('mall', mallMapType);
     map.setMapTypeId('mall');
 
-    var defaultBounds = map.getBounds();
-    var defaultCenter = map.getCenter();
-   // map.overlayMapTypes.insertAt(0, maptiler);
-    google.maps.event.addListener(map, 'drag', function (e) {
-        if (strictBounds.contains(map.getCenter())) return;
-        var c = map.getCenter(), x = c.lng(), y = c.lat(),
-            maxX = strictBounds.getNorthEast().lng(),
-            maxY = strictBounds.getNorthEast().lat(),
-            minX = strictBounds.getSouthWest().lng(),
-            minY = strictBounds.getSouthWest().lat();
-        if (x < minX) x = minX;
-        if (x > maxX) x = maxX;
-        if (y < minY) y = minY;
-        if (y > maxY) y = maxY;
-        map.setCenter(mapCenter);
-    });
-
-
-    var markerDragCallback = function (marker) {
-        return function (e) {
-            /*defaultBounds = map.getBounds();
-            console.log(defaultBounds);
-            if (!defaultBounds.contains(e.LatLng)) {
-                console.log('out of bounds');
-                //marker.LatLng = defaultCenter;
-            }*/
-        };
-    };
 
     addMarker = function (position, dragEndCallback) {
         var marker = new google.maps.Marker({
@@ -73,7 +39,6 @@ $(document).ready(function () {
             map: map,
             draggable: true
         });
-        //google.maps.event.addListener(marker, 'drag', markerDragCallback(marker));
         if (dragEndCallback != null) {
             google.maps.event.addListener(marker, 'dragend', dragEndCallback);
         }
