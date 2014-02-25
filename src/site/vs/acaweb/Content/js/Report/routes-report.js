4e34132732f94c05053303839f0b8f57f02aef7f
Enmanuel Toribio <eats007@gmail.com>
2014-02-24 20:38:26 -0400

    adding entries and outs infowindow

--- src/site/vs/acaweb/Content/js/Report/routes-report.js
+++ src/site/vs/acaweb/Content/js/Report/routes-report.js
@@ -1,28 +1,28 @@
 ﻿
-$(function () {
+$(function() {
     var currentArrows = [];
     var currentLabels = [];
+    var currentMarkers = {};
     var paths = {};
     var packagesurl = base_url + 'Packages/Get/';
     var lineSymbol = { path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW };
-
-    var printRoutes = function (newUrl, callback) {
+    var printRoutes = function(newUrl, callback) {
         $.ajax({
-            url: newUrl == null ? packagesurl : newUrl,
-            method: 'GET',
-            dataType: 'json',
-            success: function (packagesGroupedByMac) {
+            url: newUrl == null ? packagesurl : newUrl, method: 'GET', dataType: 'json',
+            success: function(packagesGroupedByMac) {
                 for (var macIdx in packagesGroupedByMac) {
-                    var lastLatLng = '';
-                    var currLatLng;
+                    var lastLatLng = ''; var currLatLng;
                     var packages = packagesGroupedByMac[macIdx];
                     for (var packageIdx in packages) {
                         var pack = packages[packageIdx];
                         currLatLng = pack.LatLng;
-                        if (lastLatLng != '') {
-                            if (typeof (paths[lastLatLng]) == "undefined") { paths[lastLatLng] = {}; }
+                        if (lastLatLng != '' && lastLatLng != currLatLng) {
+                            if (typeof (paths[lastLatLng]) == "undefined") { paths[lastLatLng] = { outs: 0, entries: 0 }; }
+                            if (typeof (paths[currLatLng]) == "undefined") { paths[currLatLng] = { outs: 0, entries: 0 }; }
                             if (typeof (paths[lastLatLng][currLatLng]) == "undefined") { paths[lastLatLng][currLatLng] = 0; }
                             paths[lastLatLng][currLatLng]++;
+                            paths[lastLatLng].outs++;
+                            paths[currLatLng].entries++;
                         }
                         lastLatLng = currLatLng;
                     }
@@ -36,50 +36,78 @@ $(function () {
                         var lineCoordinates = [startLatLng, endLatLng];
                         var labelLat = (endLatLng.lat() + startLatLng.lat()) / 2;
                         var labelLng = (endLatLng.lng() + startLatLng.lng()) / 2;
-                        var direction = "→"; 
-                       // if (startLatLng.lat() > endLatLng.lat())
-                        //    direction = "←";
-                        //
-                        var label = new MapLabel({
-                            map: map,
+                        var direction = getDirectionArrow(startLatLng, endLatLng);
+                        var label = new MapLabel({ map: map,
                             text: paths[startPoint][endPoint] + " personas " + direction,
                             position: new google.maps.LatLng(labelLat, labelLng)
                         });
                         var line = new google.maps.Polyline({
                             path: lineCoordinates,
-                            icons: [{icon: lineSymbol, offset: '100%'}],
+                            icons: [{ icon: lineSymbol, offset: '100%' }],
                             map: map, strokeWeight: 2, strokeOpacity: 0.8
                         });
                         line.label = label;
-                        google.maps.event.addListener(line, "mouseover", function () {
+                        google.maps.event.addListener(line, "mouseover", function() {
                             this.setOptions({ strokeColor: "#FF0000" });
                             this.label.set('fontColor', "#FF0000");
                         });
-                        google.maps.event.addListener(line, "mouseout", function () {
+                        google.maps.event.addListener(line, "mouseout", function() {
                             this.setOptions({ strokeColor: "#000000" });
                             this.label.set('fontColor', "#000000");
                         });
+                        if (typeof (currentMarkers[endPoint]) == "undefined") {
+                            if (typeof (paths[endPoint]) == "undefined") { paths[endPoint] = { entries: 0, outs: 0 }; }
+                            currentMarkers[endPoint] = {
+                                marker:new google.maps.Marker({ position: endLatLng, map: map }),
+                                markerInfoContent: "<p><b>Salidas: </b>" + paths[endPoint].outs + "</p><p><b>Entradas: </b>" + paths[endPoint].entries + "</p>"
+                            };
+                            currentMarkers[endPoint].marker.infowindow = new google.maps.InfoWindow({ content: currentMarkers[endPoint].markerInfoContent });
+                            google.maps.event.addListener(currentMarkers[endPoint].marker, 'click', function () { this.infowindow.open(map, this); });
+
+                        }
                         currentArrows.push(line);
                         currentLabels.push(label);
                     }
                 }
             }
-        }).fail(function (jqXHR, textStatus) { alert("Error cargando las rutas"); });
+        }).fail(function(jqXHR, textStatus) { alert("Error cargando las rutas"); });
     };
+
+
     printRoutes(null);
     var cleanVariables = function() {
         for (var arrow in currentArrows) { currentArrows[arrow].setMap(null); }
         for (var label in currentLabels) { currentLabels[label].setMap(null); }
+        for (var marker in currentMarkers) { currentMarkers[marker].marker.setMap(null); }
         paths = [];
+        currentArrows = [];
+        currentLabels = [];
+        currentMarkers = {};
     };
-    $("#btnFilter").bind('click', function () {
+    $("#btnFilter").bind('click', function() {
         cleanVariables();
         reloadReport(printRoutes, packagesurl);
     });
-    var reloadLoop = function () {
+    var reloadLoop = function() {
         cleanVariables();
         reloadReport(printRoutes, packagesurl);
         setTimeout(reloadLoop, parseInt($("#refreshingTime").val()) * 1000);
     };
-    setTimeout(reloadLoop, parseInt($("#refreshingTime").val())*1000);
+    setTimeout(reloadLoop, parseInt($("#refreshingTime").val()) * 1000);
+
+    var getDirectionArrow = function (startLatLng, endLatLng) {
+        var direction = "→";
+        if(startLatLng.lng() > endLatLng.lng()) {
+            if (startLatLng.lat() < endLatLng.lat()) { direction = "↖"; }
+            else if (startLatLng.lat() > endLatLng.lat()) { direction = "↙"; }
+            else { direction = "←"; }
+        }else if (startLatLng.lng() < endLatLng.lng()) {
+            if (startLatLng.lat() < endLatLng.lat()) { direction = "↗"; }
+            else if (startLatLng.lat() > endLatLng.lat()) { direction = "↘"; }
+        }else {
+            if (startLatLng.lat() < endLatLng.lat()) { direction = "↑"; }
+            else if (startLatLng.lat() > endLatLng.lat()) { direction = "↓"; }
+        }
+        return direction;
+    };
 });
