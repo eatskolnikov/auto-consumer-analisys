4c6dec20020905fbb02f5be32306f836ccad7d86
Enmanuel Toribio <eats007@gmail.com>
2014-04-05 01:12:40 -0400

    asdflknafin

---
+++ src/site/vs/acaweb/Controllers/BaseMapController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ACAPackagesListener.API.Models.Repositories;
+
+namespace acaweb.Controllers
+{
+    public class BaseMapController : Controller
+    {
+        protected IMapRepository mapRepository;
+
+        public BaseMapController()
+        {
+            //mapRepository = new NHMapRepository();
+            //var selected = mapRepository.GetSelected();
+            ViewBag.TilesSource = "Content/maps/8240df3408ae029b955bcd29743dec98/";// selected.TilesSource;
+        }
+    }
+}
